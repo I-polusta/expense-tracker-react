bc44edd4e7114cf5284d30f67c41e33541c5ffbb
I-polusta <polustaishan2001@gmail.com>
2021-10-24 15:44:38 +0530

    final tracker ready

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,19 @@
 export default (state, action) => {
   switch (action.type) {
+    case "DELETE__TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+
+    case "ADD__TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+
     default:
       return state;
   }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer.js";
 //Initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 //Create Context
@@ -17,10 +12,27 @@ export const GlobalContext = createContext(initialState);
 //provider component
 export const GlobalProvider = ({ children }) => {
   const [state, dispacth] = useReducer(AppReducer, initialState);
+
+  //actions
+  function deleteTransaction(id) {
+    dispacth({
+      type: "DELETE__TRANSACTION",
+      payload: id,
+    });
+  }
+  function addTransaction(transaction) {
+    dispacth({
+      type: "ADD__TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
       }}
     >
       {children}
